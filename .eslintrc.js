d1c4bc9563c76eb38b0d8671419028ed2dd4e8e6
_TrueLecter_ <andrew.panassiouk@gmail.com>
2019-12-24 17:58:13 +0200

    Adjusted eslint to work with Webstorm

---
+++ .eslintrc.js
@@ -0,0 +1,66 @@
+module.exports = {
+	env: {
+		node: true
+	},
+	'extends': [
+		'airbnb-base',
+		'plugin:import/errors',
+		'plugin:import/warnings',
+		'plugin:import/typescript',
+		'plugin:@typescript-eslint/recommended'
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: './tsconfig.json',
+		tsconfigRootDir: __dirname
+	},
+	plugins: [
+		'import',
+		'@typescript-eslint'
+	],
+	rules: {
+		'@typescript-eslint/interface-name-prefix': ['error', 'always'],
+		'@typescript-eslint/no-non-null-assertion': 'off',
+		'@typescript-eslint/indent': ['error', 'tab'],
+		'@typescript-eslint/ban-ts-ignore': 'off',
+		'@typescript-eslint/semi': ['error'],
+		'no-restricted-syntax': ['error', 'WithStatement'],
+		'comma-dangle': ['error', 'only-multiline'],
+		'max-len': ['error',
+			{
+				code: 120,
+				ignoreTemplateLiterals: true,
+				ignoreStrings: true
+			}
+		],
+		'no-param-reassign': ['error', { props: false }],
+		'import/extensions': ['error', 'ignorePackages',
+			{
+				js: 'never',
+				ts: 'never'
+			}
+		],
+		'no-dupe-class-members': 'off',
+		'no-await-in-loop': 'off',
+		'arrow-parens': 'off',
+		'no-continue': 'off',
+		'lines-between-class-members': 'off',
+		'no-tabs': ['error',
+			{
+				allowIndentationTabs: true
+			}
+		],
+		indent: 'off',
+		semi: 'off'
+	},
+	overrides: [
+		{
+			files: [
+				'{services,shared}/*/test/**/*.ts'
+			],
+			env: {
+				jest: true
+			}
+		}
+	]
+};
