27754e8d78b5d1127ea8739b82b54cf165a96077
_TrueLecter_ <andrew.panassiouk@gmail.com>
2020-01-29 11:45:45 +0200

    fix(bot): Fixed !sb command not working for legendary heroes
    Signed-off-by: _TrueLecter_ <andrew.panassiouk@gmail.com>

--- services/bot/src/embeds/HeroSBWBlockEmbed.ts
+++ services/bot/src/embeds/HeroSBWBlockEmbed.ts
@@ -1,5 +1,5 @@
 import { MessageEmbed, Message } from 'discord.js';
-import { Hero, HeroClassColors, HeroForm } from '@pandora/entities';
+import { Hero, HeroClassColors } from '@pandora/entities';
 
 import PaginationEmbed from './PaginationEmbed';
 import { splitText, imageUrl } from '../util/functions';
@@ -9,28 +9,32 @@ export default class HeroSBWBlockEmbed extends PaginationEmbed {
 	constructor(initialMessage: Message, hero: Hero, page: number | undefined) {
 		super(initialMessage);
 
-		const embeds = hero.sbws.map((sbw) => {
-			const form = hero.forms.find(f => f.star === sbw.star) as HeroForm;
+		const embeds = hero.forms.map((form) => {
+			const sbw = hero.sbws.find(s => form.star === s.star);
 
 			const embed = new MessageEmbed()
 				.setTitle(`${translate(form.name)} (${form.star}★)`)
 				.setThumbnail(imageUrl(`skills/${form.blockImage}`))
 				.addField(`${translate(form.blockName)} (Lv. ${form.skillLvl})`, translate(form.blockDescription));
 
-			const abilityChunks = splitText(translate(form.passiveDescription));
+			if (form.passiveName) {
+				const abilityChunks = splitText(translate(form.passiveDescription));
 
-			embed.addField(translate(form.passiveName), abilityChunks.shift());
+				embed.addField(translate(form.passiveName), abilityChunks.shift());
 
-			for (const abilityChunk of abilityChunks) {
-				embed.addField('\u200b', abilityChunk);
+				for (const abilityChunk of abilityChunks) {
+					embed.addField('\u200b', abilityChunk);
+				}
 			}
 
-			const sbwChunks = splitText(translate(sbw.ability));
+			if (sbw) {
+				const sbwChunks = splitText(translate(sbw.ability));
 
-			embed.addField('SBW effect', sbwChunks.shift());
+				embed.addField('SBW effect', sbwChunks.shift());
 
-			for (const sbwChunk of sbwChunks) {
-				embed.addField('\u200b', sbwChunk);
+				for (const sbwChunk of sbwChunks) {
+					embed.addField('\u200b', sbwChunk);
+				}
 			}
 
 			return embed;
