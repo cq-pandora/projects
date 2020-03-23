487dba362832950775946d33b5a3756af1eadaf0
_TrueLecter_ <andrew.panassiouk@gmail.com>
2020-03-23 09:16:04 +0200

    fix(bot): Updated bot links
    Signed-off-by: _TrueLecter_ <andrew.panassiouk@gmail.com>

--- services/bot/src/commands/links.ts
+++ services/bot/src/commands/links.ts
@@ -19,17 +19,17 @@ export class LinksCommand extends BaseCommand {
 			fields: [
 				{
 					name: 'Guides',
-					value: '[Hero skills/builds list](http://bit.ly/2NNDlbg)\n[Beginner and returning](http://bit.ly/CQNewRetGuide)\n[AFK](http://bit.ly/CQAfkGuide)',
+					value: '[Hero skills/builds list](http://bit.ly/2NNDlbg)\n[AFK](http://bit.ly/CQAfkGuide)',
 					inline: true,
 				},
 				{
 					name: 'Tier Lists',
-					value: '[kamakiller\'s SS2 Overview](https://goo.gl/yPrKBR)\n[kamakiller\'s Tier List](http://bit.ly/cqkktier)',
+					value: '[[Placeholder]]',
 					inline: true,
 				},
 				{
 					name: 'Raid',
-					value: '[kamakiller\'s Loki](http://bit.ly/CQLokiIncarnate)\n[kamakiller\'s Manacar](https://goo.gl/PbpKdG)\n[Manacar Comic](https://goo.gl/aJ8Yoy)',
+					value: '[kamakiller\'s Loki](https://bit.ly/CQLokiIncarnateReupload)\n[Manacar Comic](https://goo.gl/aJ8Yoy)',
 					inline: true,
 				},
 				{
@@ -39,12 +39,12 @@ export class LinksCommand extends BaseCommand {
 				},
 				{
 					name: 'How To Get',
-					value: '[Monuments](https://goo.gl/e10jeA)\n[Lionel\'s skin](https://goo.gl/9BXBkD)\n[Himiko\'s skin](https://goo.gl/5yDbjr)',
+					value: '[Lionel\'s skin](https://goo.gl/9BXBkD)\n[Himiko\'s skin](https://goo.gl/5yDbjr)',
 					inline: true,
 				},
 				{
 					name: 'Challenge',
-					value: '[kamakiller](https://goo.gl/bkC85j)\n[Sigils list](http://bit.ly/2NCLFu9)',
+					value: '[kamakiller](https://bit.ly/CQChallengeModeReupload)\n[Sigils list](http://bit.ly/2NCLFu9)',
 					inline: true,
 				},
 				{
@@ -54,12 +54,12 @@ export class LinksCommand extends BaseCommand {
 				},
 				{
 					name: 'Hasla Guides',
-					value: '[Comics](https://goo.gl/HPsANc)\n[Season 2](https://goo.gl/UQdjhw)\n[Berry system](https://goo.gl/jbgmLa)\n[Math in CQ](https://goo.gl/D5vtS2)',
+					value: '[Comics](https://goo.gl/HPsANc)\n[Season 2](https://goo.gl/UQdjhw)\n[Berry system](https://goo.gl/jbgmLa)',
 					inline: true,
 				},
 				{
 					name: 'Miscellaneous',
-					value: '[Leveling guide](https://i.redd.it/ya5mgw9xvfk01.jpg)\n[cq-assets](http://bit.ly/PandoraAssets)\n[block-map](http://bit.ly/CQBlockMap)',
+					value: '[Leveling guide](https://i.redd.it/ya5mgw9xvfk01.jpg)\n[cq-assets](https://github.com/cq-pandora/assets/)\n[block-map](http://bit.ly/CQBlockMap)',
 					inline: true,
 				},
 			],
