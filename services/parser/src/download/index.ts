765d8e0f462501761a8c958dbb61ad932c27c0f1
_TrueLecter_ <andrew.panassiouk@gmail.com>
2020-05-07 18:54:25 +0300

    feat(parser): Now able to download encrypted data from servers (if crc code mismatch) to Datas folder of game cache
    Signed-off-by: _TrueLecter_ <andrew.panassiouk@gmail.com>

---
+++ services/parser/src/download/index.ts
@@ -0,0 +1,1 @@
+export { default as downloadData } from './data';
--- services/parser/src/index.ts
+++ services/parser/src/index.ts
@@ -1,8 +1,10 @@
 import decrypt from './decrypt';
 import normalize from './normalization';
 import split from './split';
+import { downloadData } from './download';
 
 (async (): Promise<void> => {
+	await downloadData();
 	await decrypt();
 	await normalize();
 	await split();
