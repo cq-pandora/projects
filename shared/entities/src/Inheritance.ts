547b949798550cd11373ec67344f6c9a021e94f4
_TrueLecter_ <andrew.panassiouk@gmail.com>
2020-08-06 19:49:04 +0300

    fix(parser): Increased max inheritance level
    Signed-off-by: _TrueLecter_ <andrew.panassiouk@gmail.com>

--- shared/entities/src/Inheritance.ts
+++ shared/entities/src/Inheritance.ts
@@ -10,11 +10,12 @@ import { HeroClass } from './hero';
 export type InheritanceLevel =
 	0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 |
 	11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 |
-	21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29 | 30;
+	21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29 | 30 |
+	31 | 32 | 33 | 34 | 35;
 
 export function isInheritanceLevel(inp: number | string): inp is InheritanceLevel {
 	const res = parseInt(inp as string, 10);
-	return res >= 1 && res <= 30;
+	return res >= 1 && res <= 35;
 }
 
 export interface IInheritanceStatsOptions {
