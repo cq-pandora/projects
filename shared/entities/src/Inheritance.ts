73b09368b4013aa5ddfe11808dd7a67ad5ff0466
Andrew Panassiouk <andrew.panassiouk@gmail.com>
2021-07-25 17:03:56 +0300

    fix(entities): Upgraded inheritance level

--- shared/entities/src/Inheritance.ts
+++ shared/entities/src/Inheritance.ts
@@ -11,11 +11,11 @@ export type InheritanceLevel =
 	0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 |
 	11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 |
 	21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29 | 30 |
-	31 | 32 | 33 | 34 | 35;
+	31 | 32 | 33 | 34 | 35 | 36 | 37 | 38 | 39 | 40;
 
 export function isInheritanceLevel(inp: number | string): inp is InheritanceLevel {
 	const res = parseInt(inp as string, 10);
-	return res >= 1 && res <= 35;
+	return res >= 1 && res <= 40;
 }
 
 export interface IInheritanceStatsOptions {
