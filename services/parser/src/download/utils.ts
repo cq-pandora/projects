98a4a30857688a506a836e077d5a9e4764b75981
Andrew Panassiouk <andrew.panassiouk@gmail.com>
2021-09-12 14:35:26 +0300

    chore(parser): New files url retrieval method

--- services/parser/src/download/utils.ts
+++ services/parser/src/download/utils.ts
@@ -1,12 +1,71 @@
-import axios from 'axios';
+import { client as WebSocket } from 'websocket';
+import { v4 as uuidv4 } from 'uuid';
 import { apkVersion } from '../util';
 
-const launchingInfosUrl = (v: string) => `http://lnc.cc.nhnst.com:10080/hsp/lnc/getLaunchingInfos.json?gameNo=10329&gameClientVersion=${v}&platformSdkVersion=2.86&osType=2&osVersion=9&udid=00000000000000000000`
+const WS_ADDRESS = 'wss://gslb-gamebase-lh.cloud.toast.com:11443/lh';
 
-export async function getRootDownloadPath() {
+function retrieveURLFromWS(clientVersion: string): Promise<string> {
+	return new Promise<string>((resolve, reject) => {
+		const ws = new WebSocket({
+			webSocketVersion: 13,
+			tlsOptions: {
+				headers: {
+					'User-Agent': 'okhttp/3.12.3',
+				},
+			},
+		});
+
+		ws.on('connectFailed', reject);
+
+		ws.on('connect', (wsc) => {
+			wsc.send(JSON.stringify({
+				productId: 'launching',
+				apiId: 'getLaunching',
+				version: 'v1.3.2',
+				appId: 'yFEtSikK',
+				parameters: {
+					usimCountryCode: 'UA',
+					deviceKey: '0000000000000000',
+					clientVersion,
+					displayLanguage: 'en',
+					uuid: uuidv4(),
+					deviceCountryCode: 'GB',
+					osCode: 'AOS',
+					lastCheckedNoticeTime: 0,
+					osVersion: '9',
+					termsVersion: '53.0.2',
+					appId: 'yFEtSikK',
+					deviceLanguage: 'en',
+					sdkVersion: '2.26.0',
+					deviceModel: 'STF-L09',
+					storeCode: 'GG'
+				},
+				headers: {
+					'X-TCGB-Transaction-Id': uuidv4()
+				}
+			}));
+
+			wsc.on('message', message => {
+				if (message.type === 'utf8') {
+					const data = JSON.parse(message.utf8Data);
+
+					wsc.close();
+					resolve(data.launching.app.accessInfo.serverAddress);
+				} else {
+					wsc.close();
+					reject(new TypeError(`Bad response ${JSON.stringify(message)}`));
+				}
+			});
+		});
+
+		ws.connect(WS_ADDRESS);
+	});
+}
+
+export async function getRootDownloadPath(): Promise<string> {
 	const currentApkVersion = await apkVersion();
-	const { data: launchingInfos } = await axios.get(launchingInfosUrl(currentApkVersion));
-	const gameUrls = launchingInfos.serverInfos.GAMESVR.split(';');
+	const serverAddresses = await retrieveURLFromWS(currentApkVersion);
+	const gameUrls = serverAddresses.split(';');
 
 	return gameUrls[0];
 }
