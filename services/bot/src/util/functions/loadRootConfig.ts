2266f428a29a0f0e7cf70f1e9f49d8371c15928f
Andrew Panassiouk <andrew.panassiouk@gmail.com>
2022-01-30 23:22:25 +0200

    fix(bot): Do not require overriding all the configs now

--- services/bot/src/util/functions/loadRootConfig.ts
+++ services/bot/src/util/functions/loadRootConfig.ts
@@ -4,5 +4,9 @@ import { join as pathJoin } from 'path';
 const defaultPath = pathJoin(__dirname, '../../../');
 
 export default function loadRootConfig(path: string, configPath: string = defaultPath): string {
-	return readFileSync(pathJoin(configPath, path), { encoding: 'utf-8' });
+	try {
+		return readFileSync(pathJoin(configPath, path), { encoding: 'utf-8' });
+	} catch (e: any) {
+		return readFileSync(pathJoin(defaultPath, path), { encoding: 'utf-8' });
+	}
 }
